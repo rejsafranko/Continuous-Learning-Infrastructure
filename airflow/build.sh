031fb8e601e5d7fbb3f708852e8ab8a75818734e
Rej Šafranko <79145150+rejsafranko@users.noreply.github.com>
2025-05-31 16:59:24 +0200

    Update build.sh

--- airflow/build.sh
+++ airflow/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 export $(grep -v '^#' .env | xargs)
 
 AWS_REGION="eu-central-1"
@@ -9,23 +11,11 @@ ECR_REPO_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO_N
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REPO_URI
 
-docker build \
-    --build-arg AIRFLOW_HOME \
-    --build-arg AIRFLOW_FIRSTNAME \
-    --build-arg AIRFLOW_LASTNAME \
-    --build-arg AIRFLOW_EMAIL \
-    --build-arg AIRFLOW_PASSWORD \
-    --build-arg AWS_ACCESS_KEY \
-    --build-arg AWS_SECRET_KEY \
-    --build-arg AZURE_CONN_STRING \
-    --build-arg SNS_TARGET_ARN \
-    --build-arg AWS_CONN_ID \
-    --build-arg SNS_REGION \
-    -t ${ECR_REPO_URI}:latest .
-
 aws ecr describe-repositories --repository-names $ECR_REPO_NAME --region $AWS_REGION >/dev/null 2>&1 || \
 aws ecr create-repository --repository-name $ECR_REPO_NAME --region $AWS_REGION
 
+docker build -t ${ECR_REPO_URI}:latest .
+
 docker push ${ECR_REPO_URI}:latest
 
-echo "Docker image successfully pushed to AWS ECR: ${ECR_REPO_URI}:latest"
+echo "Docker image pushed: ${ECR_REPO_URI}:latest"
